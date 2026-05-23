6434a761fd7065ca4d144856e38a91f2fcb7c8db
Yvan Cywan <cywanyvan@gmail.com>
2026-05-23 15:44:17 +0100

    init: project structure with Gradle and add .gitignore

---
+++ src/main/java/com/studio/hebi/bot/Main.java
@@ -0,0 +1,7 @@
+package com.studio.hebi.bot;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@AnvilPluginHost
+public class Main {
+}
